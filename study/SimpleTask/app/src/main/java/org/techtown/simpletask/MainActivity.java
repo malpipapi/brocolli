2b9f853285a74097d87a52efa0c9c8cc794ae211
Dohyun822 <98006584+Dohyun822@users.noreply.github.com>
2022-02-03 17:24:10 +0900

    2week: upload studyfile

---
+++ study/SimpleTask/app/src/main/java/org/techtown/simpletask/MainActivity.java
@@ -0,0 +1,30 @@
+package org.techtown.simpletask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.techtown.simpletask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
